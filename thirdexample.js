f320a9c5b96f87c133b91732df8dc3c7eca7b273
India Byrne <byrneindia@gmail.com>
2020-02-26 14:50:38 -0700

    Examples

---
+++ thirdexample.js
@@ -0,0 +1,8 @@
+/*
+* Variables, Data Types, and Typing
+*/
+let var1 = "2";
+let var5 = "5";
+let result = var1 + var5
+
+console.log (result);
---
+++ variablesanddata.js
@@ -0,0 +1,12 @@
+/*
+* Variables, Data Types, and Typing
+*/
+let wordData;
+
+wordData = "Word List";
+
+console.log(wordData);
+
+let subtotal =100
+let tax = subtotal * 0.07;
+let totalWithTax = tax + subtotal
